62b5748d98e11ec2aac91d74356be1910527fc63
RatonRoy <royraton18@gmail.com>
2022-03-02 22:56:03 +0600

    clear ok

--- js/app.js
+++ js/app.js
@@ -2,30 +2,19 @@ document.getElementById('search-btn').addEventListener('click', function () {
 	const searchPhone = () => {
 		const searchField = document.getElementById('search-field');
 		const searchValue = searchField.value;
-		// console.log(searchValue)
+		// clear data 
 		searchField.value = '';
+		
+		// load data 
 		const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-		// console.log(url);
 		fetch(url)
 			.then(res => res.json())
 			.then(data => displayPhone(data.data));
 		// display  Phone functions 
 		const displayPhone = (phones) => {
-			// main container 
-			const mainContainer = document.getElementById('main-phone-section');
-			// section title 
-			const sectionTitle = document.createElement('div');
-			sectionTitle.classList.add('phone-section-title');
-			sectionTitle.innerHTML = `
-				<h2 class="title">featured <span class="subtitle">phones</span></h2>
-			`
-			// search result 
 			const searchResult = document.getElementById('phone-container');
-			mainContainer.insertBefore(sectionTitle, searchResult);
+			searchResult.textContent = '';
 			phones.forEach(phone => {
-				console.log(phone);
-				const phoneId = phone.slug;
-				console.log(phoneId);
 				const article = document.createElement('article');
 				article.classList.add('phone-card');
 				article.innerHTML = `
@@ -45,7 +34,7 @@ document.getElementById('search-btn').addEventListener('click', function () {
 							<div class="phone-info">
 							<p class="phone-brand"> ${phone.brand}</p>
 							<div class="phone-details">
-								<button onClick = "showDetailes()">Details</button>
+								<button onClick = "lordPhoneDetailes('${phone.slug}')">Details</button>
 								
 							</div>
 							</div>
@@ -63,8 +52,43 @@ document.getElementById('search-btn').addEventListener('click', function () {
 
 });
 
-const showDetailes = (phoneId) => {
-	// console.log('');
+const lordPhoneDetailes = (phoneId) => {
 	const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
 	console.log(url);
+	fetch(url)
+		.then(res => res.json())
+		.then(data => displayPhoneDetailes(data.data));
+}
+
+const displayPhoneDetailes = (phone) => {
+	console.log(phone);
+	const showPhoneContainer = document.getElementById('show-phone-container');
+	const div = document.createElement('div');
+	div.innerHTML = `
+		<div class="show-phone-image">
+			<img src="${phone.image}" alt="awesome phone" srcset="">
+		</div>
+		<div class="show-phone-header">
+			<h2 class="show-phone-name">${phone.name}</h2>
+			<h3 class="phone-date">${phone.releaseDate}</h3>
+		</div>
+		<div class="show-phone-information">
+			<ul>
+				<li class="list-item">${phone.mainFeatures.chipSet}</li>
+				<li class="list-item">${phone.mainFeatures.displaySize}</li>
+				<li class="list-item">${phone.mainFeatures.memory}</li>
+				<li class="list-item">${phone.mainFeatures.storage}</li>
+				<li class="list-item">Sensors
+					<ul class="list-wraper-2">
+						<li class="list-item">${phone.mainFeatures.sensors[0]}</li>
+						<li class="list-item">${phone.mainFeatures.sensors[1]}</li>
+						<li class="list-item">${phone.mainFeatures.sensors[2]}</li>
+						<li class="list-item">${phone.mainFeatures.sensors[3]}</li>
+					</ul>
+				</li>
+				<li class="list-item">${phone.slug}</li>
+			</ul>
+		</div>
+	`
+	showPhoneContainer.appendChild(div);
 }
